4655f6f6184705fa5f2fec8cbab0d7e2193e7aa0
brittshanklin <76928555+brittshanklin@users.noreply.github.com>
2023-09-14 14:38:29 -0500

    Update streamlit_app.py
    Added try-except to api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,16 +25,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # Display api response
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error('Please select a fruit to get information')
+  else: 
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-# Flatten JSON into dataframe 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display as table in streamlit
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
